07321a11d4cc0a5f04fd18dca923b54683a576db
Olivier Mountou <oliviermountou@MacBook-Pro-de-Olivier.local>
2021-05-06 15:19:55 +0200

    deleted js-cookie npm

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
 
 const Signup = () => {
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  let history = useHistory();
 
   const handleOnSubmit = async (event) => {
     event.preventDefault();
@@ -28,6 +30,8 @@ const Signup = () => {
       //PUT TOKEN ON COOKIE
 
       // REDIRECT TO HOMEPAGE
+
+      history.push("/");
     } catch (error) {
       console.log(error);
     }
